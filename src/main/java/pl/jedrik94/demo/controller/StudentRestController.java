0610c6765a9bd27e568ce639464dd5b767856e7a
Wojtkowiak Jedrzej <jedrik94@gmail.com>
2019-01-09 13:37:34 +0100

    New endpoint enabling getting specific student by list index.

--- src/main/java/pl/jedrik94/demo/controller/StudentRestController.java
+++ src/main/java/pl/jedrik94/demo/controller/StudentRestController.java
@@ -1,10 +1,12 @@
 package pl.jedrik94.demo.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import pl.jedrik94.demo.model.Student;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,13 +14,30 @@ import java.util.List;
 @RequestMapping(value = "/rest-api")
 public class StudentRestController {
 
-    @RequestMapping(value = "/students", method = RequestMethod.GET)
-    public List<Student> getStudent() {
-        List<Student> studentList = new ArrayList<>();
+    private List<Student> studentList = new ArrayList<>();
 
+    @PostConstruct
+    private void loadData() {
         studentList.add(new Student("Wojtkowiak", "Jedrzej"));
         studentList.add(new Student("Wojtkowiak", "Milosz"));
+        studentList.add(new Student("Wojtkowiak", "Hanna"));
+    }
 
+    @RequestMapping(value = "/students", method = RequestMethod.GET)
+    public List<Student> getStudents() {
         return studentList;
+    }
+
+    @RequestMapping(value = "/students/{studentId}", method = RequestMethod.GET)
+    public Student getStudent(@PathVariable int studentId) {
+        Student tmpStudent = null;
+
+        try {
+            tmpStudent = studentList.get(studentId);
+        } catch (IndexOutOfBoundsException e) {
+            System.err.println(e);
+        }
+
+        return tmpStudent;
     }
 }
